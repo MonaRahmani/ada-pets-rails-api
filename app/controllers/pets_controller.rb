780f1226cb71067a2d829f742a44168b38c426cd
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-30 13:56:02 -0800

    worked on create action-test

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,36 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all.order(:name)
-    render json: { ok: 'YESS'}, status: :ok
+    pets = Pet.all.order(:name)
+    render json: pets.as_json(only: [:id, :name, :specious, :age, :owner]), status: :ok
+  end
+
+  def show
+    pet = Pet.find_by(id: params[:id])
+
+    if pet.nil?
+      render json: {
+          ok: false,
+          message: 'not found',
+      }, status: :not_found
+      return
+    end
+    render json: pet.as_json(only: [:id, :name, :specious, :age, :owner]), status: :ok
+  end
+
+  def create
+    pet = Pet.new(pet_params)
+
+    if pet.save
+      render json: pet.as_json(only: [:id]), status: :created
+    else
+      render json: { errors: pet.errors.messages }, status: :bad_request
+    end
+
+
+  end
+
+  private
+  def pet_params
+    params.require(:pet).permit(:name, :specious, :age, :owner)
   end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,10 +1,99 @@
 require "test_helper"
 
 describe PetsController do
-  it "must get index" do
-    get pets_path
-    must_respond_with :success
-    expect(response.header['Content-Type']).must_include 'json'
+  REQUIRED_PET_ATTRS = ["id", "name", "specious", "age", "owner"].sort
+  describe "index" do
+
+    it "must get index" do
+      get pets_path
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+    end
+
+    it "will return all the proper fieldsfor a list of pets" do
+
+      get pets_path
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Array
+
+      body.each do |pet|
+        expect(pet).must_be_instance_of Hash
+        expect(pet.keys.sort).must_equal REQUIRED_PET_ATTRS
+      end
+    end
+
+    it "return an empty arr if no pets" do
+      Pet.destroy_all
+
+      get pets_path
+
+      body = JSON.parse(response.body)
+      expect(body.length).must_equal 0
+    end
   end
+
+  describe "show" do
+    it "will return hash with proper fields for an existinf pet" do
+      pet = pets(:topol)
+
+      get pet_path(pet.id)
+
+      must_respond_with :success
+
+      body = JSON.parse(response.body)
+      expect(response.header['Content-Type']).must_include 'json'
+
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal REQUIRED_PET_ATTRS
+    end
+
+    it "will return 404 request with json for non-existent pet" do
+      get pet_path(-1)
+
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body['ok']).must_equal false
+      expect(body['message']).must_equal 'not found'
+    end
+
+    describe "create" do
+      let(:pet_params) {
+        {
+            pet: {
+                name: "test_name",
+                species: "Dog",
+                age: 13,
+                owner: "test_owner"
+            }
+        }
+      }
+
+      it "can create new pet" do
+        expect{ post pets_path, params: pet_params }.must_differ "Pet.count", 1
+        must_respond_with :created
+      end
+
+      it "give bad request status when user gives bad data " do
+        pet_params[:pet][:name] = nil
+
+        expect { post pets_path, params: pet_params }.wont_change "Pet.count"
+
+        must_respond_with :bad_request
+
+        expect(response.header['Content-Type']).must_include 'json'
+        body = JSON.parse(response.body)
+        expect(body["errors"].keys).must_include "name"
+      end
+
+    end
+
+  end
+
+
+
+
+
 
 end
